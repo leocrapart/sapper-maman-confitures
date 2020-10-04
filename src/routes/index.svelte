9ed55c7145bf5becbcc7333a66f910bb3db97382
leolecrado <leocrapart@yahoo.fr>
2020-10-05 00:24:49 +0200

    6

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,27 @@
+
+<script context="module">
+	export async function preload(page) {
+		
+		const res = await this.fetch("https://api.openbrewerydb.org/breweries/")
+		const data = await res.json()
+		return { data }
+	}
+</script>
+
+<script>
+	export let data
+	console.log(data) 
+</script>
+
+<div>
+	<div>data is here</div>
+	{#each data as bear, i}
+		<div>{i}</div>
+		{#each Object.entries(bear) as bear_data}
+			<div>{bear_data[0]} => {bear_data[1]}</div>
+		{/each}
+	{/each}
+</div>
 <svelte:head>
 	<title>maman confitures</title>
 </svelte:head>
