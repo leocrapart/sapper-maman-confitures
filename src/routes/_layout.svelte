1191eb14c07b300d8b5322d5a5211d5810c22708
leolecrado <leocrapart@yahoo.fr>
2020-10-06 01:33:14 +0200

    10

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -6,14 +6,4 @@
 <main class="lg:mt-20 container mx-auto p-4">
 	<slot></slot>
 </main>
-<sapper:body>
-	<style>
-		body {
-			background-image: url('https://ik.imagekit.io/pl6zwucqxr/grass-background_tveE3-0zCxI.jpg');
-			background-position: center center;
-			background-repeat: no-repeat;
-			background-attachment: fixed;
-			background-size: cover;
-		}
-	</style>
-</sapper:body>
+
--- src/routes/info.svelte
+++ src/routes/info.svelte
@@ -1,23 +1,3 @@
 
-<script>
-	import { count } from './store.js'
-
-	function increment() {
-		count.update(n => n+1)
-	}
-
-	let count_value
-	const unsuscribe = count.subscribe(value => {
-		count_value = value
-	})
-</script>
-
-<div class="bg-white rounded p-4">
-	<div>test</div>
-	<div>Count is {count_value}</div>
-	<button on:click={increment}
-		class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >+1</button>
-	
-</div>
 
 <div>info page</div>
